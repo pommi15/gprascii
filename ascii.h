1a6b90eb64a92cc41ccb7f4bd2c1794c59a25e66
Thomas <pommi15@hotmail.com>
2016-03-23 13:14:06 +0100

    started repository

---
+++ ascii.h
@@ -0,0 +1,24 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~
+|       @)>-}-;--       |
+|      ASCII Image      |
+|          and          |
+|     Mandelbrot Set    |
+|           by          |
+|      Tobias Watzek    |
+|        if15b038       |
+|          and          |
+|     Thomas Rauhofer   |
+|        if15b029       |
+~~~~~~~~~~~~~~~~~~~~~~~*/
+
+#ifdef ASCII_H
+#define ASCII_H
+
+class Ascii {
+
+public:
+    Ascii(int width, int height;
+    ~Ascii();
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~
+|       @)>-}-;--       |
+|      ASCII Image      |
+|          and          |
+|     Mandelbrot Set    |
+|           by          |
+|      Tobias Watzek    |
+|        if15b038       |
+|          and          |
+|     Thomas Rauhofer   |
+|        if15b029       |
+~~~~~~~~~~~~~~~~~~~~~~~*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
