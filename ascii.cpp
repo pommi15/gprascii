f99c349252ee022cdef1635ae5a1272f8a8d6ed3
pommi <pommi15@hotmail.com>
2016-03-27 13:18:42 +0200

    modification to AsciiImmage

--- ascii.cpp
+++ ascii.cpp
@@ -14,26 +14,26 @@
 #include "ascii.h"
 
 Ascii::Ascii(int width, int height){
-    int x = width;
-    int y = height;
-    char colorset[] = {' ', '.', '\'', '`', '^', '"', ',', ':', ';', 'I', 'l', '!', 'i', '>', '<', '~', '+', '_', '-', '?', ']', '[' , '}', '{' , '1', ')', '(', '|', '\\', '/' , 't', 'f', 'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'};
-    char pic[width][height];
+    this->width = width;
+    this->height = height;
+    this->colourset[] = {' ', '.', '\'', '`', '^', '"', ',', ':', ';', 'I', 'l', '!', 'i', '>', '<', '~', '+', '_', '-', '?', ']', '[' , '}', '{' , '1', ')', '(', '|', '\\', '/' , 't', 'f', 'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@', '$'};
+    this->pic[width][height];
     for(int j = 0; j < height; j++){
         for(int i = 0; i < width; i++){
-            pic[x][y] = ' ';
+            this->pic[width][height] = ' ';
         }
     }
 }
 
 Ascii::~Ascii(){};
 
- void Ascii::setPix(int x, int y, int color){
-    if(color < 0) {
-        Ascii::pic[x][y] = Ascii::colorset[0];
-    }else if(color < 68) {
-        pic[x][y] = colorset[color];
+ void Ascii::setPix(int x, int y, int colour){
+    if(colour < 0) {
+        this->pic[x][y] = this->colourset[0];
+    }else if(colour < 68) {
+        this->pic[x][y] = this->colourset[colour];
     }else {
-        pic[x][y] = colorset[68];
+        this->pic[x][y] = this->colourset[68];
     }
 }
 
--- ascii.h
+++ ascii.h
@@ -15,11 +15,15 @@
 #define ASCII_H
 
 class Ascii {
-
+private:
+    int width;
+    int height;
+    char colourset[];
+    char pic[];
 public:
     Ascii(int width, int height);
     ~Ascii();
-    void setPix(int x, int y, int color);
+    void setPix(int x, int y, int colour);
     void show();
 };
 
