d143126123228e5aff755598fa9d093cdbe49f78
Thomas <pommi15@hotmail.com>
2016-03-31 15:17:15 +0200

    commit

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -17,12 +17,12 @@
 using namespace std;
 
 double scale_x(int x, double x0, double x1, int width){
-    double new_x = ((x) / (width - 1)) * (x1 - x0) + x0;
+    double new_x = ((x) / (width - 1)) * (x1 - x0) - x0;
     return new_x;
 
 }
 double scale_y(int y, double y0, double y1, int height){
-    double new_y = ((y) / (height - 1)) * (y1 - y0) + y0;
+    double new_y = ((y) / (height - 1)) * (y1 - y0) - y0;
     return new_y;
 
 }
@@ -41,7 +41,6 @@ int main()
     cin >> width;
     cout << "Enter height: ";
     cin >> height;
-    /*
     cout << "Enter x0: ";
     cin >> x0;
     cout << "Enter y0: ";
@@ -49,14 +48,15 @@ int main()
     cout << "Enter x1: ";
     cin >> x1;
     cout << "Enter y1: ";
-    cin >> y1;*/
+    cin >> y1;
     cout << "Enter maxi: ";
     cin >> maxi;
 
 
     Ascii *img = new Ascii(width, height);
-    for(int y = 0; y < height; ++y) {
-        for(int x = 0; x < width; ++x) {
+
+    for(int x = 0; x < width; ++x) {
+        for(int y = 0; y < height; ++y) {
 
             double zr = 0.;
             double zi = 0.;
